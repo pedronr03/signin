88aae6d0638e4e29555ffe4f60f784dcc3f1fc34
Pedro Reis <pedronr0309@gmail.com>
2022-09-24 12:48:39 -0300

    feat: create auth and users components

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
