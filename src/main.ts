1b47d2a1c910a14b4c110b3866332a6619b4787f
Pedro Reis <pedronr0309@gmail.com>
2022-09-24 18:52:59 -0300

    fix: port key name

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.$PORT;
+  const PORT = process.env.PORT;
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
